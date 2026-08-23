9faf54a3a48f5fd93bc4d3e4965d4d7a6c3222a8
Gaox <3230185790@qq.com>
2026-08-23 23:45:25 +0800

    完成链队

--- 03_Queue/LinkedQueue.cpp
+++ 03_Queue/LinkedQueue.cpp
@@ -30,10 +30,96 @@ typedef struct
 //q是一个结构体里面有front与rear，front指向了链队的头所以代表了链队
 //front与rear又是Qnode类型的
 //rear是Qnode结构体的指针，指向包含了data与next的节点
-void initq(Linkq &q)
+bool initq(Linkq &q)
 {
+    
     q.front=(Qnode*)malloc(sizeof(Qnode));
+
+    if(q.front==NULL)
+    {
+
+        return false;
+    }
+
     q.front->next=NULL;
     q.rear=q.front;
+    return true;
 
 }
+
+
+bool isempty(Linkq q)
+{
+    if(q.front==q.rear)
+    {
+        return true;
+    }
+}
+
+bool enqueue(Linkq &q,Elemtype e)
+{
+    Qnode *s=(Qnode*)malloc(sizeof(Qnode));
+
+    if(s==NULL)
+        return false;
+
+    s->data=e;
+    //入队是在队尾
+    s->next=NULL;
+    //在原来的队列上。rear是最后一个他的下一个指向s代表插入一个
+    q.rear->next=s;
+    //移动尾指针
+    q.rear=s;
+
+    return true;
+}
+
+bool dequeue(Linkq &q,Elemtype &e)
+{
+    if(q.front==q.rear)
+        return false;
+
+    Qnode *p=q.front->next;
+
+
+    e=p->data;
+
+    q.front->next=p->next;
+
+
+    if(p==q.rear)
+    {
+        q.rear=q.front;
+
+    }
+    //为什么出队的时候要判断 p == Q.rear
+
+
+// front
+//  ↓
+// [头] → [10]
+//          ↑
+//         rear
+
+//删除之后头节点指向null。rear不可以还指向10，所以让他等于头节点
+    free (p);
+    return true;
+}
+
+
+bool gettop(Linkq q,Elemtype &e)
+{
+    if(q.front==q.rear)
+        return false;
+
+    e=q.front->next->data;
+    return true;
+
+}
+
+
+
+
+
+
+
